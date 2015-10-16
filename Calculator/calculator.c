b2fbbe3c435ca69c3b8d6aae05e71c11b7e7b236
mark-uom <mark.micallef@um.edu.mt>
2015-10-16 13:22:10 +0200

    First 2 methods

--- Calculator/calculator.c
+++ Calculator/calculator.c
@@ -1,4 +1,16 @@
 /*
-This is a test file.
+* calculator.c
+*
+*  Created on: 16 Oct 2015
+*      Author: markmicallef
 */
+
+
+int add(int x, int y) {
+	return x + y;
+}
+
+int sub(int x, int y) {
+	return x - y;
+}
 
